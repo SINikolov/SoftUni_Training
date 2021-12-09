cac712f8c9cc425b8cb735b26a3db520004112df
SINikolov <93074954+SINikolov@users.noreply.github.com>
2021-12-09 10:13:45 +0200

    Add files via upload

---
+++ 02_drink_something.py
@@ -0,0 +1,9 @@
+years = int(input())
+if years <= 14:
+    print("drink toddy")
+elif 14 < years <= 18:
+    print("drink coke")
+elif 18< years <= 21:
+    print("drink beer")
+else:
+    print("drink whisky")
